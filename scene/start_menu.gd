a35087c47af494add24e3d53c4d6f237f694c522
Liu Ming Hang <523633637@qq.com>
2025-04-22 10:55:26 +0800

    ai 技能解锁基本逻辑

--- scene/start_menu.gd
+++ scene/start_menu.gd
@@ -7,5 +7,8 @@ func _on_settings_button_pressed():
 	# TODO: 实现设置面板
 	pass
 
+func _on_skill_tree_button_pressed():
+	get_tree().change_scene_to_file("res://scene/skill_tree.tscn")
+
 func _on_quit_button_pressed():
 	get_tree().quit()
