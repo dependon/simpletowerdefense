62fab06897b3cf46f08cb6f49da72b64075df52b
Liu Ming Hang <523633637@qq.com>
2025-05-21 11:02:17 +0800

    feat(enemy): 新增多种敌人类型及其场景资源
    在 `EnemyType` 枚举中新增了多种敌人类型，包括 `BLACKWIZARD`、`BLUEEYEDWIZARD` 等，并预加载了对应的敌人场景资源。这些新增的敌人类型将用于丰富游戏中的敌人种类和战斗体验。

--- enemy/enemy_manager.gd
+++ enemy/enemy_manager.gd
@@ -7,7 +7,17 @@ enum EnemyType {
 	THIEF,
 	WIZARD,
 	WARRIOR,
-	WEREWOLF
+	WEREWOLF,
+	BLACKWIZARD,
+	BLUEEYEDWIZARD,
+	COWBOY,
+	CRAZYONE,
+	GREENMAGE,
+	GREENWIZARD,
+	MAGEONE,
+	ORC,
+	REDWIZARD,
+	TECHKNIFEMAKER
 }
 
 # 预加载敌人场景
@@ -16,7 +26,17 @@ var enemy_scenes = {
 	EnemyType.THIEF: preload("res://enemy/enemy_thief.tscn"),
 	EnemyType.WIZARD: preload("res://enemy/enemy_wizard.tscn"),
 	EnemyType.WARRIOR: preload("res://enemy/enemy_warrior.tscn"),
-	EnemyType.WEREWOLF: preload("res://enemy/enemy_werewolf.tscn")
+	EnemyType.WEREWOLF: preload("res://enemy/enemy_werewolf.tscn"),
+	EnemyType.BLACKWIZARD: preload("res://enemy/enemy_BlackWizard_p.tscn"),
+	EnemyType.BLUEEYEDWIZARD: preload("res://enemy/enemy_BlueEyedWizard_p.tscn"),
+	EnemyType.COWBOY: preload("res://enemy/enemy_Cowboy_p.tscn"),
+	EnemyType.CRAZYONE: preload("res://enemy/enemy_CrazyOne_p.tscn"),
+	EnemyType.GREENMAGE: preload("res://enemy/enemy_GreenMage_p.tscn"),
+	EnemyType.GREENWIZARD: preload("res://enemy/enemy_GreenWizard_p.tscn"),
+	EnemyType.MAGEONE: preload("res://enemy/enemy_MageOne_p.tscn"),
+	EnemyType.ORC: preload("res://enemy/enemy_Orc_p.tscn"),
+	EnemyType.REDWIZARD: preload("res://enemy/enemy_RedWizard_p.tscn"),
+	EnemyType.TECHKNIFEMAKER: preload("res://enemy/enemy_TechKnifeMaker_p.tscn")
 }
 
 # 获取敌人场景的方法
