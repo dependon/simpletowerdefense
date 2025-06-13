66fe588160aeabf2cf72edc6ff48aa5ebda3909a
depend <523633637@qq.com>
2025-06-13 16:36:57 +0800

    feat(敌人): 更新敌人属性和碰撞类型
    - 修改CrazyOne敌人的基础速度和生命值
    - 将Cowboy_p、CrazyOne_p和BlueEyedWizard_p的节点类型从CharacterBody2D改为Area2D
    - 更新所有敌人角色的贴图资源

--- enemy/enemy_CrazyOne_p.gd
+++ enemy/enemy_CrazyOne_p.gd
@@ -1,8 +1,8 @@
 extends Enemy
 
 # 蓝眼巫师特有属性
-@export var base_speed: float = 120.0  # 基础移动速度
-@export var base_hp: int = 200       # 基础生命值
+@export var base_speed: float = 60.0  # 基础移动速度
+@export var base_hp: int = 5000       # 基础生命值
 
 func _ready() -> void:
 	speed = base_speed
